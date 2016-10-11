092208910e2be69ab7e7bfd1a8fb880086f65a5b
mbr84 <matthew.bramfeld@gmail.com>
2016-10-11 11:27:31 -0700

    object type

--- server.js
+++ server.js
@@ -9,3 +9,28 @@ import {
   GraphQLString,
   GraphQLInt,
 } from 'graphql';
+
+const goldbergType = new GraphQLObjectType({
+  name: 'Goldberg',
+  description: 'Member of the Goldberts',
+  fields: {
+    character: GraphQLString,
+    description: 'Name of character',
+  },
+  actor: {
+    type: GraphQLString,
+    description: 'Actor playing the character',
+  },
+  role: {
+    type: GraphQLString,
+    description: 'Family Role',
+  },
+  traits: {
+    type: GraphQLString,
+    description: 'Character traits',
+  },
+  id: {
+    type: GraphQLInt,
+    description: 'Id of this Goldberg',
+  },
+});
